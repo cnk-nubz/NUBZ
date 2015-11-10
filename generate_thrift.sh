7f586d5827c92c1d56923191df5a5d63440ce7fb
Krzysztof Lewko <krzys.minib00m@gmail.com>
2015-11-10 21:59:56 +0100

    [T3] Such directory rework, wow

--- generate_thrift.sh
+++ generate_thrift.sh
@@ -3,8 +3,8 @@
 thrift -r -out server/src/communication --gen cpp communication/server.thrift 
 rm server/src/communication/Server_server.skeleton.cpp 
 
-thrift -r -out www/prj/cnk/strona/thrift_structures --gen py communication/server.thrift
-mv www/prj/cnk/strona/thrift_structures/server/Server-remote www/prj/cnk/strona/thrift_structures/
+thrift -r -out www/cnk/strona/thrift_structures --gen py communication/server.thrift
+mv www/cnk/strona/thrift_structures/server/Server-remote www/cnk/strona/thrift_structures/
 
 thrift -r -out android/prj/app/src/main/java --gen java communication/server.thrift 
 
