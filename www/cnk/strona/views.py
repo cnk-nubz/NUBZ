0ac7f17d9de1cf4034a9bd792cfbfa51b7726a0d
Krzysztof Lewko <krzys.minib00m@gmail.com>
2015-11-14 21:52:53 +0100

    [T15] Get images urls from server

--- www/cnk/strona/views.py
+++ www/cnk/strona/views.py
@@ -1,4 +1,5 @@
 import sys
+import json
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext, loader
@@ -7,6 +8,17 @@ from ThriftCommunicator import ThriftCommunicator
 def index(request):
 	tc = ThriftCommunicator()
 	ret = tc.getMapImages()
+	ret_version = ret.version
+	ret_urls = ret.levelImageUrls
+	url_floor0 = "floorplan0.jpg"
+	url_floor1 = "floorplan1.jpg"
+	if ret in ret_urls.keys():
+		url_floor0 = ret_urls[0]
+	if ret in ret_urls.keys():
+		url_floor1 = ret_urls[1]
 	template = loader.get_template('index.html')
-	context = RequestContext(request)
+	context = RequestContext(request, {
+	"url_floor0" : url_floor0,
+	"url_floor1" : url_floor1,
+	})
 	return HttpResponse(template.render(context))
