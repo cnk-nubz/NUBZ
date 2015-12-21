0b07ea97b13196ec0060d409f6128fdf52c104f5
Michał Majewski <mm347108@students.mimuw.edu.pl>
2015-12-21 06:30:37 +0100

    [T37] reorganized tiles files, removing old maps

--- server/src/Config.h
+++ server/src/Config.h
@@ -21,9 +21,11 @@ public:
     std::uint16_t databasePort;
 
     std::string urlPrefixForMapImage;
+    std::string urlPrefixForMapImageTiles;
 
     std::string publicFolderPath;
     std::string tmpFolderPath;
+    std::string mapTilesFolderPath;
 
 private:
     static const std::string argServerPort;
@@ -34,9 +36,11 @@ private:
     static const std::string argDatabasePort;
 
     static const std::string argUrlPrefixForMapImage;
+    static const std::string argUrlPrefixForMapImageTiles;
 
     static const std::string argPublicFolderPath;
     static const std::string argTmpFolderPath;
+    static const std::string argMapTilesFolderPath;
 
     void loadFromFile(const std::string &path);
     void validate() const;
