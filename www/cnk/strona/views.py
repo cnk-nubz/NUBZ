2296ae74e1c32072692b86eb3b683abb202be423
ThatKorean <krzys.minib00m@gmail.com>
2016-03-20 19:43:22 +0100

    [T111] Added separate /android subpage for all platforms

--- www/cnk/strona/views.py
+++ www/cnk/strona/views.py
@@ -109,6 +109,9 @@ def getMapPage(request, file, activeLink):
 	})
 	return HttpResponse(template.render(context))
 
+def androidApp(request):
+    return HttpResponseRedirect("/static/android_app/example")
+
 @ensure_csrf_cookie
 def index(request):
 	if request.META.get("HTTP_USER_AGENT", '').lower().find("android") > 0:
